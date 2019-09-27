37efa27533f1c23b72309462beeb0fa68956e1bb
Jiuyang liu <liujiuyang1994@gmail.com>
2019-09-27 05:20:23 +0000

    (maybe) fix compile failed in mill 0.5.1:
    current master version will compile failed by
    "mill.eval.Evaluator does not take type parameters"
    This change `Evaluator[Any]` to `Evaluator`
    and explicitly give the `crossVersion` in `build.sh`

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 # Proof-of-concept mill build that can be used with ammonite.
 # TODO: make this single file and add maven version
 
-mill diagrammer.assembly
-cp out/diagrammer/assembly/dest/out.jar diagrammer.jar
+# test success in mill 0.5.1 
+mill "diagrammer[2.12.7].assembly"
+cp out/diagrammer/2.12.7/assembly/dest/out.jar diagrammer.jar
 echo "./firrtl-diagrammer is ready to run"
