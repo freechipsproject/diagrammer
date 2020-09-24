3dc6cacc1f70ca07f54f7e1109784bf5d68795ec
chick <chick@qrhino.com>
2020-09-24 12:50:34 -0700

    This PR moves project to Apache 2.0 LICENSE All source files have been changed to use SPDX license: https://spdx.org/licenses/ Also changed build.sbt and build.sc

--- build.sbt
+++ build.sbt
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 name := "firrtl-diagrammer"
 
 resolvers ++= Seq(
--- src/main/scala/dotvisualizer/dotnodes/DotNode.scala
+++ src/main/scala/dotvisualizer/dotnodes/DotNode.scala
@@ -1,3 +1,5 @@
+// SPDX-License-Identifier: Apache-2.0
+
 package dotvisualizer.dotnodes
 
 import scala.collection.mutable.ArrayBuffer
